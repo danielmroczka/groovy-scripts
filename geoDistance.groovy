9a760432f79e8f422e1606b226339b6025f24208
daniel <daniel.mroczka@gmail.com>
2015-04-19 01:20:15 +0200

    added groovy scripts

---
+++ geoDistance.groovy
@@ -0,0 +1,24 @@
+/**
+ * Computes distance in kilometers between point1 (lat1, long1) and point2 (lat2, long2)
+ *
+ * @param lat1
+ * @param long1
+ * @param lat2
+ * @param long2
+ * @return distance in kilometers
+ */
+double dist(double lat1, double long1, double lat2, double long2) {
+    d2r = Math.PI / 180.0
+    def MEAN_RADIUS = 6371
+
+    Math.with {
+        double dlong = (long2 - long1) * d2r;
+        double dlat = (lat2 - lat1) * d2r;
+        double a = pow(sin(dlat / 2.0), 2) + cos(lat1 * d2r) * cos(lat2 * d2r) * pow(sin(dlong / 2.0), 2);
+        double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        double d = MEAN_RADIUS * c;
+        return d;
+    }
+}
+
+println dist(50, 20, 49, 19)
---
+++ gpx.groovy
@@ -0,0 +1,100 @@
+/**
+ * Reads gpx files in provided directory. Parses gpx file and uses maps.googleapis.com/maps/api/geocode to find street name and postcode
+ */
+import groovy.json.JsonSlurper
+import groovy.io.FileType
+
+def dir = new File('j:/download/gpx/')
+
+Set zip = []
+Set street = []
+
+dir.eachFile(FileType.FILES) { file ->
+    if (!file.name.endsWith('.gpx')) {
+        return
+    }
+    println "Processing file: " + file
+
+
+    def log = new File(dir, file.name.replace('.gpx', '.log'))
+    log.text = ''
+
+
+    def xml = new XmlSlurper().parseText(file.text)
+
+
+
+    def lastPos
+
+    xml.trk.trkseg.trkpt.each { trkpt ->
+
+
+        if (lastPos && dist(trkpt, lastPos) < 50) {
+            return
+        }
+
+        def res = printAddress(trkpt).results[0].address_components
+
+        def streetname = res.findAll {
+            it.types.contains('route')
+        }.collect { it.long_name }
+
+        def postalcode = res.findAll { it.types.contains('postal_code') }.collect({ it.long_name })
+
+        log.append(postalcode[0] + ', ' + streetname[0] + '\n')
+
+        zip.add(postalcode[0])
+        street.add(streetname[0])
+
+        lastPos = trkpt
+
+        print '.'
+    }
+
+    // File newFile = file.name.replace('.gpx', '.bkp')
+    //  file.renameTo(new File(file.name.replace('.gpx', '.bkp')))
+}
+
+def printAddress(trkpt) {
+
+    def status = false
+
+    def jsonSlurper = new JsonSlurper()
+    def object
+    while (!status) {
+        String html = ('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + trkpt.@lat + ',' + trkpt.@lon).toURL().text
+        object = jsonSlurper.parseText(html)
+        status = object.status == 'OK'
+        if (!status) {
+            sleep(5)
+            // println object.status
+        }
+    }
+
+    return object
+}
+
+double dist(trkpt1, trkpt2) {
+    def long1 = Double.parseDouble(trkpt1.@lon.text())
+    def long2 = Double.parseDouble(trkpt2.@lon.text())
+    def lat1 = Double.parseDouble(trkpt1.@lat.text())
+    def lat2 = Double.parseDouble(trkpt2.@lat.text())
+
+    d2r = Math.PI / 180.0
+
+    Math.with {
+        double dlong = (long2 - long1) * d2r;
+        double dlat = (lat2 - lat1) * d2r;
+        double a = pow(sin(dlat / 2.0), 2) + cos(lat1 * d2r) * cos(lat2 * d2r) * pow(sin(dlong / 2.0), 2);
+        double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        double d = 6367000 * c;
+        return d;
+    }
+}
+
+println ''
+println zip
+println street
+println ''
+println 'Done'
+
