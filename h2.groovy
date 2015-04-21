94760f7128f352819437521702e8f08b00864dbc
yale <danielmroczka@users.noreply.github.com>
2015-04-21 13:38:15 +0200

    Create h2.groovy

---
+++ h2.groovy
@@ -0,0 +1,11 @@
+@GrabConfig(systemClassLoader = true)
+@Grab('com.h2database:h2:1.4.186')
+import groovy.sql.Sql
+
+def sql = Sql.newInstance("jdbc:h2:hello", "sa", "sa", "org.h2.Driver")
+// DDL
+sql.execute("CREATE TABLE IF NOT EXISTS FOO (id int, value text)")
+
+//DML
+sql.execute("INSERT INTO FOO VALUES(:id, :value)", [id: 1, value: 'hello'])
+println sql.rows("select * from foo")
