13f3eb857b413127e594f560a0017b5d686795d0
yale <danielmroczka@users.noreply.github.com>
2015-04-22 09:06:19 +0200

    Update generateIds.groovy

--- generateIds.groovy
+++ generateIds.groovy
@@ -1,11 +1,16 @@
 /**
- * Creates orderd list list of int seperated by comma. Useful in select * from table in (1,2,...,n)
+ * Creates orderd list with integers seperated by comma. Useful in select * from table in (1,2,...,n)
  */
 
-def start = 25834436
+def start = 1
 def count = 10000
+def prefix = ''
+def suffix = ''
+
+print prefix
 
 for (i in start..start + count - 1) {
     print i + ","
 }
-print start + count
+
+print start + count + suffix
