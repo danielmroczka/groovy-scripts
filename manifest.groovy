2689cc84bc7e0985eea566a30c1170c2465c250d
yale <danielmroczka@users.noreply.github.com>
2015-05-29 14:57:38 +0200

    Create manifest.groovy

---
+++ manifest.groovy
@@ -0,0 +1,29 @@
+import java.util.jar.JarFile  
+import groovy.io.FileType
+  
+public static void main(String[] args) {
+    if (args.length == 0) {
+        list('.')
+    } else {
+        list(args[0])
+    }
+}
+    
+def list(String root) {
+    def list = []
+    
+    new File(root).eachFileRecurse (FileType.FILES) { file ->
+        if(file.name.endsWith('.jar') || file.name.endsWith('.war')) {
+          list << file
+        }
+    }
+    
+    list.each {
+      println 'File: \t' + it.path 
+      println  ''
+      def attr = new JarFile(it.path).manifest.mainAttributes
+      println attr
+      println '-----------------------'
+    }
+}
+
